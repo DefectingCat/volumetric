0089c8f0714e423749c8d7d041e822b89235dcf4
xfy <i@rua.plus>
2025-03-03 15:18:18 +0800

    add fps controller

--- src/main.rs
+++ src/main.rs
@@ -1,39 +1,95 @@
+use std::f32::consts::TAU;
+
 use bevy::{
     core_pipeline::Skybox,
-    pbr::{FogVolume, VolumetricFog, VolumetricLight},
+    gltf::{GltfMesh, GltfNode},
+    pbr::VolumetricLight,
     prelude::*,
+    render::camera::Exposure,
+    window::CursorGrabMode,
 };
+use bevy_fps_controller::controller::*;
 use bevy_rapier3d::prelude::*;
+
+const SPAWN_POINT: Vec3 = Vec3::new(0.0, 1.625, 0.0);
 
 fn main() {
     App::new()
         .add_plugins(DefaultPlugins)
         .add_plugins(RapierPhysicsPlugin::<NoUserData>::default())
         .add_plugins(RapierDebugRenderPlugin::default())
+        .add_plugins(FpsControllerPlugin)
         .insert_resource(AmbientLight::default())
+        .insert_resource(ClearColor(Color::linear_rgb(0.83, 0.96, 0.96)))
         .add_systems(Startup, setup)
+        .add_systems(Update, (manage_cursor, scene_colliders, respawn))
         .run();
 }
 
 /// Initializes the scene.
-fn setup(mut commands: Commands, asset_server: Res<AssetServer>) {
-    // Spawn the glTF scene.
-    commands.spawn(SceneRoot(asset_server.load(
-        GltfAssetLabel::Scene(0).from_asset("models/Exhibition1-V1/model.gltf"),
-    )));
+fn setup(mut commands: Commands, mut window: Query<&mut Window>, asset_server: Res<AssetServer>) {
+    let mut window = window.single_mut();
+    window.title = String::from("Minimal FPS Controller Example");
 
-    // Spawn the camera.
-    commands
-        .spawn(Skybox {
-            image: asset_server.load("environment_maps/pisa_specular_rgb9e5_zstd.ktx2"),
-            brightness: 1000.0,
-            ..default()
+    let height = 3.0;
+    let logical_entity = commands
+        .spawn((
+            Collider::cylinder(height / 2.0, 0.5),
+            // A capsule can be used but is NOT recommended
+            // If you use it, you have to make sure each segment point is
+            // equidistant from the translation of the player transform
+            // Collider::capsule_y(height / 2.0, 0.5),
+            Friction {
+                coefficient: 0.0,
+                combine_rule: CoefficientCombineRule::Min,
+            },
+            Restitution {
+                coefficient: 0.0,
+                combine_rule: CoefficientCombineRule::Min,
+            },
+            ActiveEvents::COLLISION_EVENTS,
+            Velocity::zero(),
+            RigidBody::Dynamic,
+            Sleeping::disabled(),
+            LockedAxes::ROTATION_LOCKED,
+            AdditionalMassProperties::Mass(1.0),
+            GravityScale(0.0),
+            Ccd { enabled: true }, // Prevent clipping when going fast
+            Transform::from_translation(SPAWN_POINT),
+            LogicalPlayer,
+            FpsControllerInput {
+                pitch: -TAU / 12.0,
+                yaw: TAU * 5.0 / 8.0,
+                ..default()
+            },
+            FpsController {
+                air_acceleration: 80.0,
+                ..default()
+            },
+        ))
+        .insert(CameraConfig {
+            height_offset: -0.5,
         })
-        .insert(VolumetricFog {
-            // This value is explicitly set to 0 since we have no environment map light
-            ambient_intensity: 0.0,
+        .id();
+    commands.spawn((
+        Camera3d::default(),
+        Projection::Perspective(PerspectiveProjection {
+            fov: TAU / 5.0,
             ..default()
-        });
+        }),
+        Exposure::SUNLIGHT,
+        RenderPlayer { logical_entity },
+    ));
+    commands.spawn(Skybox {
+        image: asset_server.load("environment_maps/pisa_specular_rgb9e5_zstd.ktx2"),
+        brightness: 1000.0,
+        ..default()
+    });
+    // .insert(VolumetricFog {
+    //     // This value is explicitly set to 0 since we have no environment map light
+    //     ambient_intensity: 0.0,
+    //     ..default()
+    // });
 
     // Add the spot light
     commands.spawn((
@@ -49,27 +105,93 @@ fn setup(mut commands: Commands, asset_server: Res<AssetServer>) {
         VolumetricLight,
     ));
 
-    // Add the fog volume.
-    commands.spawn((
-        FogVolume::default(),
-        Transform::from_scale(Vec3::splat(35.0)),
-    ));
+    // Spawn the glTF scene.
+    commands.insert_resource(MainScene {
+        handle: asset_server.load("models/Playground/playground.glb"),
+        is_loaded: false,
+    });
 
-    // Add a camera so we can see the debug-render.
-    commands.spawn((
-        Camera3d::default(),
-        Transform::from_xyz(-3.0, 3.0, 10.0).looking_at(Vec3::ZERO, Vec3::Y),
-    ));
+    // commands.spawn(SceneRoot(asset_server.load(
+    //     GltfAssetLabel::Scene(0).from_asset("models/Exhibition1-V1/model.gltf"),
+    // )));
+}
 
-    /* Create the ground. */
-    commands
-        .spawn(Collider::cuboid(100.0, 0.1, 100.0))
-        .insert(Transform::from_xyz(0.0, -2.0, 0.0));
+fn respawn(mut query: Query<(&mut Transform, &mut Velocity)>) {
+    for (mut transform, mut velocity) in &mut query {
+        if transform.translation.y > -50.0 {
+            continue;
+        }
 
-    /* Create the bouncing ball. */
-    commands
-        .spawn(RigidBody::Dynamic)
-        .insert(Collider::ball(0.5))
-        .insert(Restitution::coefficient(0.7))
-        .insert(Transform::from_xyz(0.0, 4.0, 0.0));
+        velocity.linvel = Vec3::ZERO;
+        transform.translation = SPAWN_POINT;
+    }
+}
+
+#[derive(Resource)]
+struct MainScene {
+    handle: Handle<Gltf>,
+    is_loaded: bool,
+}
+
+fn scene_colliders(
+    mut commands: Commands,
+    mut main_scene: ResMut<MainScene>,
+    gltf_assets: Res<Assets<Gltf>>,
+    gltf_mesh_assets: Res<Assets<GltfMesh>>,
+    gltf_node_assets: Res<Assets<GltfNode>>,
+    mesh_assets: Res<Assets<Mesh>>,
+) {
+    if main_scene.is_loaded {
+        return;
+    }
+
+    let gltf = gltf_assets.get(&main_scene.handle);
+
+    if let Some(gltf) = gltf {
+        let scene = gltf.scenes.first().unwrap().clone();
+        commands.spawn(SceneRoot(scene));
+        for node in &gltf.nodes {
+            let node = gltf_node_assets.get(node).unwrap();
+            if let Some(gltf_mesh) = node.mesh.clone() {
+                let gltf_mesh = gltf_mesh_assets.get(&gltf_mesh).unwrap();
+                for mesh_primitive in &gltf_mesh.primitives {
+                    let mesh = mesh_assets.get(&mesh_primitive.mesh).unwrap();
+                    commands.spawn((
+                        Collider::from_bevy_mesh(
+                            mesh,
+                            &ComputedColliderShape::TriMesh(TriMeshFlags::all()),
+                        )
+                        .unwrap(),
+                        RigidBody::Fixed,
+                        node.transform,
+                    ));
+                }
+            }
+        }
+        main_scene.is_loaded = true;
+    }
+}
+
+fn manage_cursor(
+    btn: Res<ButtonInput<MouseButton>>,
+    key: Res<ButtonInput<KeyCode>>,
+    mut window_query: Query<&mut Window>,
+    mut controller_query: Query<&mut FpsController>,
+) {
+    for mut window in &mut window_query {
+        if btn.just_pressed(MouseButton::Left) {
+            window.cursor_options.grab_mode = CursorGrabMode::Locked;
+            window.cursor_options.visible = false;
+            for mut controller in &mut controller_query {
+                controller.enable_input = true;
+            }
+        }
+        if key.just_pressed(KeyCode::Escape) {
+            window.cursor_options.grab_mode = CursorGrabMode::None;
+            window.cursor_options.visible = true;
+            for mut controller in &mut controller_query {
+                controller.enable_input = false;
+            }
+        }
+    }
 }
